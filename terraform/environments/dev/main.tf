99f10849afb1ce93e3e6c4bb66d316801ca11c7d
Gadiel Kaleb <js@whosup.us>
2020-04-28 09:57:56 -0300

    inicio da criacao tabela bookinks

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -5,3 +5,10 @@ module "users" {
   read_capacity = 1
   jwt_secret = "${var.jwt_secret}"
 }
+
+module "name" {
+  source  = "../../infra/bookings"
+  environment = "${var.environment}"
+  write_capacity = 1
+  read_capacity = 1
+}
